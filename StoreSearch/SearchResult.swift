073e8d7affde27ee7484ee5f8181ec62e5f8ad61
Wicasta Lovelace <wicasta@icloud>
2021-09-23 13:11:43 -0400

    Add a search bar and table view. The search puts fake results in the table for now.

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Wicasta Lovelace on 9/23/21.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
